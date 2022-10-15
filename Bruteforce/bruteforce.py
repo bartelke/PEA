3f84c2af1336ad3584b1521e9d05f29277a98ecb
Bartosz Kloc <atlf75800@gmail.com>
2022-10-15 11:04:47 +0200

    working list of all possible ways

--- Bruteforce/bruteforce.py
+++ Bruteforce/bruteforce.py
@@ -1,8 +1,23 @@
 import itertools
 
 f = open("dane.txt", "r")
-print(f.readline())
-print(f.readline())
+# wczytanie ilosci wezlow (miast)
+nodesNumber = int(f.readline())
 
-# print(list(itertools.permutations(
-#     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10)))
+# lista miast ktora bedziemy permutowac
+way = []
+for i in range(1, nodesNumber):
+    way.append(i)
+
+print(way)
+# zbior wszystkich mozliwych sciezek (zbior wszystkich permutacji)
+allPosibilities = list(itertools.permutations(way, nodesNumber-1))
+
+# dodanie wezla (miasta) poczatkowego i koncowego:
+for i in range(len(allPosibilities)):
+    singlePossibility = list(allPosibilities[i])
+    singlePossibility.append(0)
+    singlePossibility.insert(0, 0)
+    allPosibilities[i] = singlePossibility
+
+print(allPosibilities)
