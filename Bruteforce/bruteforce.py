079bd10aaeeff1fe2514d7afc89e6cd191f5861c
Bartosz Kloc <atlf75800@gmail.com>
2022-10-18 13:01:43 +0200

    simple update: siplier data reading and less amount of loops

--- Bruteforce/bruteforce.py
+++ Bruteforce/bruteforce.py
@@ -1,8 +1,14 @@
 import itertools
 
-f = open("dane.txt", "r")
-# wczytanie ilosci wezlow (miast)
-nodesNumber = int(f.readline())
+# wczytanie danych
+data = []
+with open("dane.txt") as f:
+    # wczytanie ilosci wezlow (miast)
+    nodesNumber = int(f.readline())
+
+    for i in range(nodesNumber+1):
+        singleData = [int(x) for x in f.readline().split()]
+        data.append(singleData)
 
 # lista miast ktora bedziemy permutowac
 way = []
@@ -12,25 +18,16 @@ for i in range(1, nodesNumber):
 # zbior wszystkich mozliwych sciezek (zbior wszystkich permutacji)
 allPosibilities = list(itertools.permutations(way, nodesNumber-1))
 
-# dodanie wezla (miasta) poczatkowego i koncowego:
-for i in range(len(allPosibilities)):
+# algorytm:
+minCost = int(50000)
+
+for i in range(len(allPosibilities)):  # <- sprawdzam po kolei permutacje
+    # dodanie wezla 0 na poczatku i na koncu kazdej sciezki:
     singlePossibility = list(allPosibilities[i])
     singlePossibility.append(0)
     singlePossibility.insert(0, 0)
     allPosibilities[i] = singlePossibility
 
-# wczytanie danych
-data = []
-with open("dane.txt") as f:
-    for i in range(nodesNumber+1):
-        singleData = [int(x) for x in f.readline().split()]
-        data.append(singleData)
-data.pop(0)
-
-# algorytm:
-minCost = int(50000)
-
-for i in range(len(allPosibilities)):  # <- sprawdzam po kolei permutacje
     singleCost = int(0)
     # sprawdzam koszt jednej sciezki
     for j in range(len(allPosibilities[i])-1):
