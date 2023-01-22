a5c9c926cd396ea251725b5ce6c4534e42fe2842
Bartosz Kloc <atlf75800@gmail.com>
2023-01-22 16:39:10 +0100

    changed parameters in ACO (eg generations)

--- ACO/main.py
+++ ACO/main.py
@@ -170,26 +170,27 @@ with open("config.ini") as configFile:
             trueResult = int(line[4])
             pheromoneType = int(line[5])
 
-            print(alpha)
 
             loadData(pathToFile)
 
 
-rank = len(cities)
+            rank = len(cities)
+            print(rank)
 
-# ant_count: int, generations: int, alpha: float, beta: float, rho: float, q: int, strategy: int
-startTime = time.time()
-aco = ACO(10, 100, alpha, betha, 0.5, 10, pheromoneType)
-graph = Graph(matrix, rank)
-path, result = aco.solve(graph)
-print(result)
-differencePertentage = round(abs(trueResult-result)*100/trueResult, 2)
-endTime = time.time()
-totalTime = round(endTime - startTime, 3)
-print(str(differencePertentage) + "% ")
 
-# zapis wynikow do pliku:
-resultsFile = open("results.csv", "a")
-resultsFile.write("\n\n")
-resultsFile.write(str(pathToFile) + ", " + str(result) + ", " + str(totalTime) + ", " +
-                              str(startTime) + ", " + str(differencePertentage) + "% \n")
+            # ant_count: int, generations: int, alpha: float, beta: float, rho: float, q: int, strategy: int
+            startTime = time.time()
+            aco = ACO(rank, 10, alpha, betha, 0.5, 10, pheromoneType)
+            graph = Graph(matrix, rank)
+            path, result = aco.solve(graph)
+            print(result)
+            differencePertentage = round(abs(trueResult-result)*100/trueResult, 2)
+            endTime = time.time()
+            totalTime = round(endTime - startTime, 3)
+            print(str(differencePertentage) + "% ")
+
+            # zapis wynikow do pliku:
+            resultsFile = open("results.csv", "a")
+            resultsFile.write("\n\n")
+            resultsFile.write(str(pathToFile) + ", " + str(result) + ", " +
+                            str(round(totalTime, 2)) + ", " + str(differencePertentage) + "% \n")
